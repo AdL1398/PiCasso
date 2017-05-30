5271937b4d6972215cc700b7fab467e653e76430
AdL1398 <adisorn.lert@gmail.com>
2017-05-30 17:04:30 +0100

    add sendingFile function

--- source/modules/producer.py
+++ source/modules/producer.py
@@ -30,6 +30,7 @@ from pyndn import InterestFilter
 from pyndn.security import KeyChain
 from pprint import pprint
 from termopi import termopi # class with dictionary data structure
+from enumerate_publisher import EnumeratePublisher
 
 class Producer(object):
     def __init__(self, namePrefix, DS):
@@ -41,6 +42,7 @@ class Producer(object):
         self.DataStore = DS.readDataStore_json()
         self.script_path = os.path.abspath(__file__) # i.e. /path/to/dir/foobar.py
         self.script_dir = os.path.split(self.script_path)[0] #i.e. /path/to/dir/
+        self.Datamessage_size = 8000 #8kB --> Max Size from NDN standard
 
         #print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
         #pprint(self.DataStore)
@@ -94,7 +96,7 @@ class Producer(object):
                 rel_path = "PIstatus/pi_status.json"
                 abs_file_path = os.path.join(self.script_dir, rel_path)
                 print "File path of monitoring Pi:%s" %abs_file_path
-                self.sendingFile()
+                self.sendingFile(abs_file_path, interest, face)
 
         else:
             print "content type is mismatch"
@@ -103,74 +105,50 @@ class Producer(object):
         print "Register failed for prefix", prefix.toUri()
         self.isDone = True
 
-    # def sendingFile(self,face):
-    #     print "Sending File Function"
-    #     interestName = interest.getName()
-    #     interestNameSize = interestName.size()
-    #     interest_name_components = interestName.toUri().split("/")
-    #     timeStamp = interest_name_components[interest_name_components.index("service_monitoring") + 2]
-    #     print "Receive Interest message name: %s" % interestName
-    #
-    #     ##### V2 Data message is JSON file
-    #     try:
-    #         SegmentNum = (interestName.get(interestNameSize - 1)).toSegment()
-    #         dataName = interestName.getSubName(0, interestNameSize - 1)
-    #
-    #     # If no segment number is included in the INTEREST, set the segment number as 0 and set the file name to configuration script to be sent
-    #     except RuntimeError as e:
-    #         SegmentNum = 0
-    #         dataName = interestName
-    #
-    #     # Put JSON file to the Data message
-    #
-    #     jsonfileName = 'status'+'-'+self.node_id+'.json'
-    #     print 'Monitoring FileName %s' %jsonfileName
-    #     putgetfunc.put_PiID(self.pi_status, self.node_id)
-    #     putgetfunc.put_hardResources_cpu(self.pi_status, "A 1.2GHz 64-bit quad-core ARMv8 CPU")
-    #     putgetfunc.put_hardResources_mem(self.pi_status, "1GB")
-    #     putgetfunc.put_hardResources_disk(self.pi_status, "16GB")
-    #     putgetfunc.put_resourceUsage_cpu(self.pi_status, picheck.pi_cpuUsage())
-    #     putgetfunc.put_resourceUsage_cpuLoad(self.pi_status, picheck.pi_cpuLoad())
-    #     putgetfunc.put_resourceUsage_mem(self.pi_status, picheck.pi_memUsage())
-    #     self.pi_status = dockerctl.container_info(self.pi_status)
-    #     putgetfunc.put_PiID(self.pi_status, self.node_id)
-    #     putgetfunc.create_jsonFile(self.pi_status, jsonfileName)
-    #
-    #     # get the practical limit of the size of a network-layer packet : 8800 bytes
-    #     maxNdnPacketSize = self.face.getMaxNdnPacketSize()
-    #
-    #     #print 'practical limit of the size of a network-layer packet :' + str(maxNdnPacketSize)
-    #
-    #     try:
-    #         # due to overhead of NDN name and other header values; NDN header overhead + Data packet content = < maxNdnPacketSize
-    #         # So Here segment size is hard coded to 5000 KB.
-    #         # Class Enumerate publisher is used to split large files into segments and get a required segment ( segment numbers started from 0)
-    #
-    #         dataSegment, last_segment_num = EnumeratePublisher(jsonfileName, 8000, SegmentNum).getFileSegment()
-    #
-    #         # create the DATA name appending the segment number
-    #         dataName = dataName.appendSegment(SegmentNum)
-    #
-    #         data = Data(dataName)
-    #         data.setContent(dataSegment)
-    #
-    #         # set the final block ID to the last segment number
-    #         last_segment = (Name.Component()).fromNumber(last_segment_num)
-    #         data.getMetaInfo().setFinalBlockId(last_segment)
-    #         hourMilliseconds = 600 * 1000
-    #         data.getMetaInfo().setFreshnessPeriod(hourMilliseconds)
-    #
-    #         # currently Data is signed from the Default Identitiy certificate
-    #         self.keyChain.sign(data, self.keyChain.getDefaultCertificateName())
-    #         # Sending Data message
-    #         face.send(data.wireEncode().toBuffer())
-    #         print "Replied to Interest name: %s" % interestName.toUri()
-    #         print "Replied with Data name: %s" % dataName.toUri()
-    #
-    #         # If configuration manager has sent the last segment of the file, script can be stopped.
-    #         # if SegmentNum == last_segment_num:
-    #         # self.isDone = True
-    #
-    #     except ValueError as err:
-    #         print "ERROR: %s" % err
-    #
+    def sendingFile(self, file_path, interest, face):
+        print "Sending File Function"
+        interestName = interest.getName()
+        interestNameSize = interestName.size()
+        #interest_name_components = interestName.toUri().split("/")
+        #timeStamp = interest_name_components[interest_name_components.index("service_monitoring") + 2]
+        #print "Receive Interest message name: %s" % interestName
+        ##### V2 Data message is JSON file
+        try:
+            SegmentNum = (interestName.get(interestNameSize - 1)).toSegment()
+            dataName = interestName.getSubName(0, interestNameSize - 1)
+
+        # If no segment number is included in the INTEREST, set the segment number as 0 and set the file name to configuration script to be sent
+        except RuntimeError as e:
+            SegmentNum = 0
+            dataName = interestName
+        # Put file to the Data message
+        try:
+            # due to overhead of NDN name and other header values; NDN header overhead + Data packet content = < maxNdnPacketSize
+            # So Here segment size is hard coded to 5000 KB.
+            # Class Enumerate publisher is used to split large files into segments and get a required segment ( segment numbers started from 0)
+            dataSegment, last_segment_num = EnumeratePublisher(file_path, self.Datamessage_size, SegmentNum).getFileSegment()
+            # create the DATA name appending the segment number
+            dataName = dataName.appendSegment(SegmentNum)
+            data = Data(dataName)
+            data.setContent(dataSegment)
+
+            # set the final block ID to the last segment number
+            last_segment = (Name.Component()).fromNumber(last_segment_num)
+            data.getMetaInfo().setFinalBlockId(last_segment)
+            hourMilliseconds = 600 * 1000
+            data.getMetaInfo().setFreshnessPeriod(hourMilliseconds)
+
+            # currently Data is signed from the Default Identitiy certificate
+            self.keyChain.sign(data, self.keyChain.getDefaultCertificateName())
+            # Sending Data message
+            face.send(data.wireEncode().toBuffer())
+            print "Replied to Interest name: %s" % interestName.toUri()
+            print "Replied with Data name: %s" % dataName.toUri()
+
+            # If configuration manager has sent the last segment of the file, script can be stopped.
+            # if SegmentNum == last_segment_num:
+            # self.isDone = True
+
+        except ValueError as err:
+            print "ERROR: %s" % err
+
