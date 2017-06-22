128ab113b189ef0027cb9b021ca0661818572378
AdL1398 <adisorn.lert@gmail.com>
2017-06-22 16:26:57 +0100

    add service manger class

--- source/modules/test_serviceManager.py
+++ source/modules/test_serviceManager.py
@@ -2,31 +2,36 @@ from consumer import Consumer
 from consumerThread import consumerThread
 #import threading
 from threading import Timer, Thread, Event
+import sys
+import traceback
 import os
 import time
 
-print "Test Consumer"
-f = os.popen('date +%s')
-timestamp = f.read()
-print 'Timestamp %s' % timestamp
-namePrefix1 = '/picasso/monitoring/SEG_1/'
-namePrefix2 = '/picasso/monitoring/SEG_2/'
+class ServiceManaer(object):
+    def __init__(self):
+        self.namePrefix1 = '/picasso/monitoring/SEG_1/'
+        self.namePrefix2 = '/picasso/monitoring/SEG_2/'
 
-#thread1 = consumerThread(1, "Thread-1", 1, namePrefix1)
-#thread2 = consumerThread(2, "Thread-2", 2, namePrefix2)
+    def run(self):
+        try:
+            #instantiate DB here
+            #instantiate Grafana
+            # Create Thread
+            stopFlag = Event()
+            thread1 = consumerThread(1, "Thread-1", 1, self.namePrefix1, stopFlag)
+            thread1.start()
 
-stopFlag = Event()
-thread1 = consumerThread(1, "Thread-1", 1, namePrefix1, stopFlag)
-#thread2 = consumerThread(2, "Thread-2", 2, namePrefix2, stopFlag)
+        except RuntimeError as e:
+            print "ERROR: %s" %  e
 
-#thread = MyThread(stopFlag)
-thread1.start()
-#thread2.start()
-# this will stop the timer
-#stopFlag.set()
+        return True
 
-#thread1.start()
-#thread1.start()
-#thread2.start()
+if __name__ == '__main__':
 
-
+    #nameInput  = raw_input('Enter Name of Content ')
+    print 'Start Service Manager'
+    try:
+        ServiceManaer().run()
+    except:
+        traceback.print_exc(file=sys.stdout)
+        sys.exit(1)
